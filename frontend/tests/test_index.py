0af41b43a66a8bcfbe7900a5e230ff929051ff35
EndoRay <landodude27@gmail.com>
2017-10-09 15:22:07 -0700

    Update test_index.py

--- frontend/tests/test_index.py
+++ frontend/tests/test_index.py
@@ -9,3 +9,7 @@ def test_can_connect():
 def test_homepage_shows():
     r = req.get('http://localhost:5000')
     assert 'Home' in r.text
+
+    
+def rooms():
+return render_template("templates/rooms.html")
