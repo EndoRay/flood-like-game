da0d1c5b4c089a51abebee70042add46acf33a29
EndoRay <landodude27@gmail.com>
2017-10-09 15:34:46 -0700

    Update run.py

--- frontend/run.py
+++ frontend/run.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route ("/rooms")
+def rooms ():
+    return render_template("templates/rooms.html")
+
+
 if __name__ == '__main__':
     app.run()
