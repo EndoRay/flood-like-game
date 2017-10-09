0d5efe640f065eecf6e274bb786d562b85ffaccb
EndoRay <landodude27@gmail.com>
2017-10-09 15:41:53 -0700

    Update run.py

--- frontend/run.py
+++ frontend/run.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/rooms")
 def rooms():
     return render_template("rooms.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
